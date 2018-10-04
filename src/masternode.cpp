0599a6bb082bfcf74d4cb3859a9189d3b4a530e2
methuselahdev <methuselahdev@gmail.com>
2018-10-03 18:14:01 -0700

    updated lastpaid on masternodelist

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -452,8 +452,8 @@ int CMasternodePayments::LastPayment(CMasternode& mn)
     int ret = mn.GetMasternodeInputAge();
 
     BOOST_FOREACH(CMasternodePaymentWinner& winner, vWinning){
-        if(winner.vin == mn.vin && chainActive.Tip()->nHeight - winner.nBlockHeight < ret)
-            ret = chainActive.Tip()->nHeight - winner.nBlockHeight;
+        if(winner.vin == mn.vin && chainActive.Tip()->nHeight - winner.nBlockHeight < ret) 
+	    return chainActive.Tip()->GetBlockTime() - chainActive[winner.nBlockHeight]->GetBlockTime();
     }
 
     return ret;
