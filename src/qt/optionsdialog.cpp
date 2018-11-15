ce5ced53c04920772fe247b36db4d6181c7e8ab3
methuselahdev <methuselahdev@gmail.com>
2018-11-14 17:04:21 -0800

    theme update to match general theme

--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -116,7 +116,7 @@ OptionsDialog::OptionsDialog(QWidget *parent, bool enableWallet) :
         }
     }
 #if QT_VERSION >= 0x040700
-    ui->thirdPartyTxUrls->setPlaceholderText("https://example.com/tx/%s");
+    ui->thirdPartyTxUrls->setPlaceholderText("https://explorer.methuselahcoin.io/#/tx/%s");
 #endif
 
     ui->unit->setModel(new MethuselahUnits(this));
